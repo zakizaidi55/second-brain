adc7789bbb1f148e64eeb73a4d179a7259df504c
zakizaidi55 <zakizaidi55@gmail.com>
2025-02-10 16:25:24 +0530

    initial commit

---
+++ brainly-be/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+app.post("/api/v1/signin", (req, res) => {
+
+})
+
+app.post("/api/v1/signin", (req, res) => {
+
+})
+
+app.get("/api/v1/content", (req, res) => {
+
+})
+
+app.delete("/api/v1/content", (req, res) => {
+
+})
+
+app.post("/api/v1/brain/share", (req, res)=> {
+
+})
+
+app.listen(3000);
